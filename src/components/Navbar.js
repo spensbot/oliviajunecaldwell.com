cb5007986b1dbbdf6a1bea2010befc930fc58c7c
Spenser Saling <spenser0saling@gmail.com>
2020-11-25 21:27:36 -0800

    Fix sass error. Change footer and Navbar.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <p style={{fontSize: '3rem'}}>Livi</p>
             </Link>
             {/* Hamburger menu */}
             <div
@@ -69,12 +69,12 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/blog">
                 Blog
               </Link>
-              <Link className="navbar-item" to="/contact">
+              {/* <Link className="navbar-item" to="/contact">
                 Contact
               </Link>
               <Link className="navbar-item" to="/contact/examples">
                 Form Examples
-              </Link>
+              </Link> */}
             </div>
             <div className="navbar-end has-text-centered">
               <a
